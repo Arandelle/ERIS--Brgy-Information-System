0473bd8876c985ab8014d70826f312d3e8d77af2
Arandelle <topaguintsarandell@gmail.com>
2024-05-12 22:12:51 +0800

    Sidebar update sm and md

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { SidebarData } from "./SidebarData";
 import ArrowRightIcon from "@mui/icons-material/ArrowRight";
 
-function Sidebar({ isOpen, toggleSideBar }) {
+function Sidebar({ isOpen, toggleSidebar }) {
   const [openSubMenu, setOpenSubMenu] = useState(null);
 
   const handleMenuItemClick = (val) => {
@@ -18,9 +18,18 @@ function Sidebar({ isOpen, toggleSideBar }) {
   };
 
   return (
-    <div className={`${isOpen ? "hidden" : "sm: absolute md:relative z-50 sm: w-4/6 md:w-1/6 uppercase"}`}>
-      <ul className="flex flex-col space-y-3 space-x-3 text-sm font-medium bg-white h-screen shadow-sm text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
-        <h1 className="text-base pt-4 text-center font-bold text-gray-700 text-nowrap">
+    <div className={`flex fixed inset-0 z-50 ${isOpen ? "md:relative md:w-1/6" : "hidden"}`}>
+    <div className="bg-black bg-opacity-25 w-full md:hidden md:w-0"></div> {/* This is the overlay */}
+    <div className="absolute md:relative left-0 w-4/5 md:w-60 bg-white h-full shadow-lg">
+      <div className="flex justify-end p-2">
+        <button onClick={toggleSidebar} className="md:hidden text-gray-600 hover:text-gray-800">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </button>
+      </div>
+      <ul className="flex flex-col space-y-3 space-x-3 text-sm font-medium h-screen overflow-y-auto text-gray-500 dark:text-gray-400 md:me-4 mb-4 md:mb-0">
+        <h1 className="text-base text-center font-bold text-gray-700 text-nowrap">
           Admin Panel
         </h1>
         {SidebarData.map((val, key) => (
@@ -64,6 +73,8 @@ function Sidebar({ isOpen, toggleSideBar }) {
         ))}
       </ul>
     </div>
+  </div>
+
   );
 }
 
