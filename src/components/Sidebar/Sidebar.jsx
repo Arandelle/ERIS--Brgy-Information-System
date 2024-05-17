8172179a4926d00cf79c3f5d91a805dc5ff8fe1d
Arandelle <topaguintsarandell@gmail.com>
2024-05-17 21:51:24 +0800

    update for heading of sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -21,10 +21,12 @@ function Sidebar({ isOpen, toggleSidebar }) {
     <div className={`flex fixed inset-0 z-50 ${isOpen ? "md:hidden md:w-1/6 md:relative" : "hidden"}
     ${!isOpen ? "md:flex md:relative" : ""}`}>
     <div className="bg-black bg-opacity-25 w-full md:relative md:w-0" ></div> {/* This is the overlay */}
-    <div className="absolute md:relative left-0 w-4/5 md:w-60 shadow-lg overflow-y-auto py-5 px-3 h-screen bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-      <ul className="space-y-2">
-        <div className="text-center">
-          <button type="button" onClick={toggleSidebar} data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" className="md:hidden text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
+    <div className="absolute md:relative w-4/5 md:w-60 shadow-lg overflow-y-auto px-3 h-screen bg-white border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+      <ul className=" space-y-2">
+        <div className="text-center space-y-2">
+          <button type="button" 
+          onClick={toggleSidebar} 
+          className="md:hidden text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" >
             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             <span class="sr-only">Close menu</span>
           </button>
