8be4895ea1e0f35819c259e4d09ecbae5fb377c2
Arandelle <topaguintsarandell@gmail.com>
2024-05-17 11:58:15 +0800

    update for notification

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,68 +1,67 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 
-export default function Login({ setAuth }) {
+export default function Login({setAuth}) {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPass] = useState("");
   const [error, setError] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (email === "admin@example.com" && password === "password123") {
-      setAuth(true);
-      navigate('/dashboard');
-    } else {
-      setError("Invalid email or password");
-    }
-  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    email === "admin@example.com" && password === "password123" ? 
+    (setAuth(true),navigate("/dashboard")) 
+    : setError("Invalid email or password");
+  }
 
   return (
-    <main className="flex h-screen w-full items-center justify-center bg-gray-100 dark:bg-gray-800 px-4">
-      <div className="w-full max-w-md">
-        <div className="space-y-1">
-          <h1 className="text-2xl dark:text-gray-300 text-center">Welcome back Admin</h1>
-          <h2 className="dark:text-gray-400">Email: admin@example.com</h2>
-          <h2 className="dark:text-gray-400">Password: password123  </h2><br></br>
-        </div>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="space-y-2">
-            <label htmlFor="email" className="dark:text-gray-400">Email</label>
-            <input
+    <>
+      <main className="flex h-screen w-full items-center justify-center bg-gray-100 dark:bg-gray-800 px-4">
+        <div className="w-full max-w-md">
+          <div className="space-y-1">
+            <h1 className="text-2xl text-center dark:text-gray-300">Welcome Admin</h1>
+            <h2 className="dark:text-gray-400">Email: admin@example.com</h2>
+            <h2 className="dark:text-gray-400">Password: password123</h2>
+          </div>
+          <form action="" onSubmit={handleSubmit} className="space-y-4">
+            <div className="space-y-2">
+              <label htmlFor="email" className="dark:text-gray-400">Email:</label>
+              <input 
+              className="w-full border border-gray-300 rounded p-2"
+              type="email"
               id="email"
               placeholder="email@example.com"
               required
-              type="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full border border-gray-300 rounded p-2"
-            />
-          </div>
-          <div className="space-y-2">
-            <div className="flex items-center justify-between">
-              <label htmlFor="password" className="dark:text-gray-400">Password</label>
-              <a className="text-sm underline dark:text-green-400" href="#">
-                Forgot password?
-              </a>
+              onChange={(e) => setEmail(e.target.value)} />
             </div>
-            <input
-              id="password"
-              placeholder="Your Password"
-              required  
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="dark:text-gray-400">Password:</label>
+                <a href="#" className="text-sm underline dark:text-green-400">
+                  Forget Password?
+                </a>
+              </div>
+              <input 
               className="w-full border border-gray-300 rounded p-2"
-            />
-          </div>
-          {error && <p className="text-red-500">{error}</p>}
-          <div>
-            <button className="w-full bg-primary text-white text-bold p-2 rounded" type="submit">
-              Sign in
-            </button>
-          </div>
-        </form>
-      </div>
-    </main>
-  );
+              type="password"
+              id="password"
+              placeholder="Type your password"
+              required
+              value={password}
+              onChange={(e) => setPass(e.target.value)}
+               />
+            </div>
+            {error && <p className="text-red-500">{error}</p>}
+            <div>
+              <button className="w-full bg-primary text-white text-bold p-2 rounded"
+              type="submit">
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </main>
+    </>
+  )
 }
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,55 +1,58 @@
 import { Toggle } from "../hooks/Toggle";
+import { useEffect, useRef } from 'react';
 import '../index.css'
 
 const Notification = ()=>{
-
+  const dropdownRef = useRef(null);
     const {isOpen, toggleDropdown} = Toggle();
 
     return (
-        <div className="relative">
-          <button
-            onClick={toggleDropdown}         
-            type="button"
-            data-dropdown-toggle="notification-dropdown"
-            className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-          >
-            <span className="sr-only">View notifications</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="currentColor"
-              viewBox="0 0 14 20"
+        <div>
+          <div className="relative">
+            <button
+              onClick={toggleDropdown}
+              type="button"
+              data-dropdown-toggle="notification-dropdown"
+              className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
             >
-              <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
-            </svg>
-          </button>
-
+              <span className="sr-only">View notifications</span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="currentColor"
+                viewBox="0 0 14 20"
+              >
+                <path d="M12.133 10.632v-1.8A5.406 5.406 0 0 0 7.979 3.57.946.946 0 0 0 8 3.464V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C1.867 13.018 0 13.614 0 14.807 0 15.4 0 16 .538 16h12.924C14 16 14 15.4 14 14.807c0-1.193-1.867-1.789-1.867-4.175ZM3.823 17a3.453 3.453 0 0 0 6.354 0H3.823Z" />
+              </svg>
+            </button>
+          
+          </div>
           {isOpen && (
-            <div
-              className="absolute z-10 right-0 mt-2 w-80 origin-top-right bg-white rounded-md shadow-lg ring-1 ring-black ring-opacity-5 divide-y divide-gray-100 focus:outline-none"
-              role="menu"
-              aria-orientation="vertical"
-              aria-labelledby="options-menu"
-            >
-              {/* Notification items go here */}
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
+              <div
+                className="absolute z-10 right-2 mt-2 w-60 md:w-80 bg-white dark:bg-gray-700 rounded-md shadow-lg"
+                role="menu"
+                aria-orientation="vertical"
+                aria-labelledby="options-menu"
               >
-                Notification 1
-              </a>
-              <a
-                href="#"
-                className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                role="menuitem"
-              >
-                Notification 2
-              </a>
-              {/* Add more notification items as needed */}
-            </div>
-          )}
+                {/* Notification items go here */}
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600 hover:rounded-md"
+                  role="menuitem"
+                >
+                  Notification 1
+                </a>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-600  hover:rounded-md"
+                  role="menuitem"
+                >
+                  Notification 2
+                </a>
+                {/* Add more notification items as needed */}
+              </div>
+            )}
         </div>
       );
 }
