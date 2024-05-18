1c47cb81498ddbce03c71bbb4ccb5c77f7c17fb9
Arandelle <topaguintsarandell@gmail.com>
2024-05-18 16:51:31 +0800

    login message update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -37,20 +37,22 @@ const Dashboard = ({setAuth}) => {
     <div className="flex flex-col w-full">
       <Header toggleSideBar={toggleDropdown} />
       {message && (
-        <div
-          className="flex absolute z-50 left-1/2 items-center p-2 mb-4 mt-2 text-sm text-green-500 rounded-lg bg-green-200 dark:bg-gray-600 dark:text-green-400"
-          role="alert"
-      >
-          <svg
-            className="flex-shrink-0 inline w-4 h-4 me-3"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-        >
-            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
-          </svg>
-          {message}
+        <div className="flex justify-center items-center">
+          <div
+            className="flex absolute z-50 justify-center items-center p-2 mb-4 mt-4 text-sm text-green-500 rounded-lg bg-green-200 dark:bg-gray-600 dark:text-green-400"
+            role="alert"
+                >
+            <svg
+              className="flex-shrink-0 inline w-4 h-4 me-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+          >
+              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+            </svg>
+            {message}
+          </div>
         </div>
       )}
       <div className="flex w-full">
