85e54cf31d0a7c1b2c80c4934239e8d5de03fb86
Arandelle <topaguintsarandell@gmail.com>
2024-05-19 21:00:04 +0800

    update for toggle dark mode button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -103,18 +103,6 @@ const Header = ({ toggleSideBar }) => {
             </svg>
             <span class="sr-only">Search</span>
           </button>
-          <label class="inline-flex items-center cursor-pointer">
-            <input
-              onClick={toggleTheme}
-              type="checkbox"
-              value=""
-              class="sr-only peer"
-            />
-            <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="ms-3 sr-only text-sm font-medium text-gray-900 dark:text-gray-300">
-              Choose Theme
-            </span>
-          </label>
           <div class="relative mr-3 hidden md:block">
             <form action="#" method="GET" class="hidden lg:block lg:pl-2">
               <label for="topbar-search" class="sr-only">
@@ -147,13 +135,33 @@ const Header = ({ toggleSideBar }) => {
                   placeholder="Search"
                 />
               </div>
-            </form>
+            </form>         
           </div>
-          <div className="flex items-center space-x-2">
+          <label className="inline-flex items-center cursor-pointer">
+          <input
+            onClick={toggleTheme}
+            type="checkbox"
+            checked={theme === 'dark'}
+            className="sr-only peer"
+          />
+          {/* <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div> */}
+          <span  className="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-gray-400 dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
+            {theme === 'light' ? (
+               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+               <path fillRule="evenodd" d="M11.675 2.015a.998.998 0 0 0-.403.011C6.09 2.4 2 6.722 2 12c0 5.523 4.477 10 10 10 4.356 0 8.058-2.784 9.43-6.667a1 1 0 0 0-1.02-1.33c-.08.006-.105.005-.127.005h-.001l-.028-.002A5.227 5.227 0 0 0 20 14a8 8 0 0 1-8-8c0-.952.121-1.752.404-2.558a.996.996 0 0 0 .096-.428V3a1 1 0 0 0-.825-.985Z" clipRule="evenodd"/>
+             </svg>
+            ) : ( 
+               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+               <path fillRule="evenodd" d="M13 3a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0V3ZM6.343 4.929A1 1 0 0 0 4.93 6.343l1.414 1.414a1 1 0 0 0 1.414-1.414L6.343 4.929Zm12.728 1.414a1 1 0 0 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 1.414 1.414l1.414-1.414ZM12 7a5 5 0 1 0 0 10 5 5 0 0 0 0-10Zm-9 4a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2H3Zm16 0a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2h-2ZM7.757 17.657a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414Zm9.9-1.414a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM13 19a1 1 0 1 0-2 0v2a1 1 0 1 0 2 0v-2Z" clipRule="evenodd"/>
+             </svg>
+            )}
+          </span>
+        </label>
+  
             <Notification />
             {/* <Menu /> */}
             <Profile setAuth={setAuth} />
-          </div>
+      
         </div>
       </div>
     </div>
