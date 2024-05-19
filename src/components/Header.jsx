f64f814465304fbb1764dc7784ac831c82a512f3
Arandelle <topaguintsarandell@gmail.com>
2024-05-19 12:38:39 +0800

    Header - update for search input

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -47,28 +47,21 @@ const Header = ({ toggleSideBar }) => {
           </a>
         </div>
         <div className="flex items-center lg:order-2">
-          <button
-            id="toggleSidebarMobileSearch"
-            type="button"
-            className="p-2 text-gray-500 rounded-lg lg:hidden hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span className="sr-only">Search</span>
-            <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-          >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-          </button>
+        <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" class="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1">
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+      </svg>
+      <span class="sr-only">Search</span>
+    </button>
+    <div class="relative hidden md:block">
+      <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+        </svg>
+        <span class="sr-only">Search icon</span>
+      </div>
+      <input type="text" id="search-navbar" class="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search..."/>
+    </div>
          
           <div className="flex items-center space-x-2">
             <Notification />
--- src/components/MyCalendar.jsx
+++ src/components/MyCalendar.jsx
@@ -34,41 +34,48 @@ const MyCalendar = () => {
 
   return (
     <div className="flex flex-col w-full">
-       <Header toggleSideBar={toggleDropdown} />
+      <Header toggleSideBar={toggleDropdown} />
       <div className="flex">
-      <div className="fixed z-50">
-        <Sidebar isOpen={isOpen} toggleSidebar={toggleDropdown} />
-      </div>
-        <div className={`w-screen p-4 dark:bg-gray-800 dark:text-white rounded-md mx-6 my-5 md:mx-8 md:my-5 ${isOpen ? 'ml-10' : 'md:ml-64  '}`}>  
+        <div className="fixed z-50">
+          <Sidebar isOpen={isOpen} toggleSidebar={toggleDropdown} />
+        </div>
+        <div
+          className={`w-screen p-4 dark:bg-gray-800 dark:text-white rounded-md mx-6 my-5 md:mx-8 md:my-5 ${
+            isOpen ? "ml-10" : "md:ml-64  "
+          }`}
+        >
           <div className="grid justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-15 my-15 gap-4">
-              <input
-              className="w-full px-4 py-2 dark:text-white dark:bg-gray-600 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400"
-                type="text"
-                placeholder="Event Title"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-        
-              />
-                <input
-                 className="w-full relative z-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:bg-gray-600"
-                  type="text"
-                  onFocus={(e) => (e.target.type = 'date')}
-                  onBlur={(e) => (e.target.type = 'text')}
-                  placeholder="Start Date"
-                  value={startDate}
-                  onChange={(e) => setStartDate(e.target.value)}
-                />
-                <input
-                 className="date-input w-full relative z-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:bg-gray-600  "
-                  type="text"
-                  placeholder="End Date"
-                  value={endDate}
-                  onFocus={(e) => (e.target.type = 'date')}
-                  onBlur={(e) => (e.target.type = 'text')}
-                  onChange={(e) => setEndDate(e.target.value)}
-                />
-          <button onClick={handleAddEvent}
-          className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:bg-blue-600">Add Event</button>
+            <input
+              className="w-full px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-50  dark:text-white dark:bg-gray-600"
+              type="text"
+              placeholder="Event Title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <input
+              className="w-full relative z-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-50 dark:bg-gray-600"
+              type="text"
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}
+              placeholder="Start Date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+            <input
+              className="date-input w-full relative z-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-50 dark:bg-gray-600  "
+              type="text"
+              placeholder="End Date"
+              value={endDate}
+              onFocus={(e) => (e.target.type = "date")}
+              onBlur={(e) => (e.target.type = "text")}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+            <button
+              onClick={handleAddEvent}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
+            >
+              Add Event
+            </button>
           </div>
           <Calendar
             localizer={localizer}
@@ -76,10 +83,10 @@ const MyCalendar = () => {
             startAccessor="start"
             endAccessor="end"
             style={{
-                height: 600,
-                marginTop: 15,
-                marginBottom: 15,
-             }}
+              height: 600,
+              marginTop: 15,
+              marginBottom: 15,
+            }}
           />
         </div>
       </div>
