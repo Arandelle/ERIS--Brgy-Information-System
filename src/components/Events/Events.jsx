ba9b3e9649591265e8aa0d46bb83c81c1cef2ad0
Arandelle <139554068+Arandelle@users.noreply.github.com>
2024-06-01 20:47:05 +0800

    Update Events.jsx having reusable header and sidebar

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react'
-import Header from '../Header'
-import Sidebar from '../Sidebar/Sidebar'
+import HeaderSidebar from '../ReusableComponents/HeaderAndSidebar'
 import { Toggle } from '../../hooks/Toggle'
 import Skeleton from '../ReusableComponents/Skeleton'
 import Maintenance from '../ReusableComponents/Maintenance'
@@ -9,22 +8,9 @@ const Events = () => {
     const {isOpen, toggleDropdown}= Toggle();
     const [loading, setLoading] = useState(true);
     return (
-      <div className='w-full flex-col flex'>
-        <Header toggleSideBar={toggleDropdown}/>
-        <div className='flex'>
-            <div className='fixed z-50'>
-              <Sidebar isOpen={isOpen} toggleSidebar={toggleDropdown}/>
-            </div>
-            <div className={`w-full ${isOpen ? "ml-0" : "md:ml-60"}`}>
-            <div className='m-3'>
-              {loading ? (<Skeleton loading={loading} setLoading={setLoading}/>) :
-                (
-                  <Maintenance title={"Events"}/>
-                )}
-            </div>
-            </div>
-        </div>      
-      </div>
+        <HeaderSidebar child={
+                  <Maintenance title={"Events"}/>}
+                />
     )
 }
 
