a4ec068f9a4e935ad3d3c182d1f6e2b932019da1
Arandelle <139554068+Arandelle@users.noreply.github.com>
2024-06-02 00:19:03 +0800

    Update Events.jsx

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,17 +1,22 @@
-import React, {useState} from 'react'
-import HeaderSidebar from '../ReusableComponents/HeaderSidebar'
-import { Toggle } from '../../hooks/Toggle'
-import Skeleton from '../ReusableComponents/Skeleton'
-import Maintenance from '../ReusableComponents/Maintenance'
+import React, { useState } from 'react';
+import HeaderSidebar from '../ReusableComponents/HeaderSidebar';
+import { Toggle } from '../../hooks/Toggle';
+import Skeleton from '../ReusableComponents/Skeleton';
+import Maintenance from '../ReusableComponents/Maintenance';
 
 const Events = () => {
-    const {isOpen, toggleDropdown}= Toggle();
+    const { isOpen, toggleDropdown } = Toggle();
     const [loading, setLoading] = useState(true);
+
     return (
         <HeaderSidebar child={
-                  <Maintenance title={"Events nga ba"}/>}
-                />
-    )
-}
+            loading ? (
+                <Skeleton loading={loading} setLoading={setLoading} />
+            ) : (
+                <Maintenance title={"Events nga ba"} />
+            )
+        } />
+    );
+};
 
-export default Events
+export default Events;
