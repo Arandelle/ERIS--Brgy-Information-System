809d142dfbfc0721eb22af83dc31f779f5631f53
Arandelle <topaguintsarandell@gmail.com>
2024-06-02 16:53:16 +0800

    History - reusable header and sidebar

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,34 +1,15 @@
 import React, { useState } from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar/Sidebar";
-import { Toggle } from "../hooks/Toggle";
 import Maintenance from "./ReusableComponents/Maintenance";
-import Skeleton from "./ReusableComponents/Skeleton";
+import HeadSide from "./ReusableComponents/HeaderSidebar";
 
 function History() {
-  const { isOpen, toggleDropdown } = Toggle();
-  const [loading, setLoading] = useState(true)
 
   return (
-    <div className="flex flex-col w-full">
-      <Header toggleSideBar={toggleDropdown} />
-      <div className="flex">
-        <div className="fixed z-50">
-          <Sidebar isOpen={isOpen} toggleSidebar={toggleDropdown} />
-        </div>
-        <div
-          className={`w-full transition-all duration-300 ease-in-out ${
-            isOpen ? "ml-0" : "md:ml-60"
-          }`}
-        >
-          <div className="m-3">
-            {loading ? (<Skeleton setLoading={setLoading}/> ): (
-             <Maintenance title={"History"}/>
-            )}
-          </div>
-        </div>
+    <HeadSide child={
+      <div className="m-3">
+      <Maintenance title={"History"}/>
       </div>
-    </div>
+    }/>
   );
 }
 
