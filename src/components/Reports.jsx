25efdb8482963079b95ce523632666aa5e706554
Arandelle <topaguintsarandell@gmail.com>
2024-06-02 16:51:13 +0800

    Reports - reusable header and sidebar

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,50 +1,37 @@
 import React from "react";
-import {useState, useEffect} from "react";
-import Header from "./Header";
-import Sidebar from "./Sidebar/Sidebar";
-import { Toggle } from "../hooks/Toggle";
-import Skeleton from './ReusableComponents/Skeleton';
+import HeadSide from "./ReusableComponents/HeaderSidebar";
+import InputReusable from "./ReusableComponents/InputReusable"
+import BtnReusable from "./ReusableComponents/BtnReusable"
 
 const Reports = () => {
-  const { isOpen, toggleDropdown } = Toggle();
-  const [loadings, setLoading] = useState(true);
-  
   return (
-    <div className="flex flex-col w-full">
-      <Header toggleSideBar={toggleDropdown} />
-      <div className="flex">
-        <div className="fixed z-50">
-          <Sidebar isOpen={isOpen} toggleSidebar={toggleDropdown} />
-        </div>
-        <div
-          className={`w-full transition-all duration-300 ease-in-out ${
-            isOpen ? "ml-0" : "md:ml-60"
-          }`}
-        >
-          <div className="m-3">
-            {loadings ? (<Skeleton setLoading={setLoading}/> ): (
-              <div>
-               <div className="grid justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-15 my-15 gap-4">
-            <input
-              className="w-full px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-50 dark:text-white dark:bg-gray-600"
-              type="date"
-              placeholder="Event Title"
-            />
-            <input
-              className="w-full relative z-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-50 dark:bg-gray-600"
-              type="date"
+    <HeadSide
+      child={
+        <div className="m-3">
+          <div className="grid justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-15 my-15 gap-4">
+            <InputReusable
+              type="text"
               placeholder="Start Date"
+              // value={}
+              // onChange={}
+              onFocus={(e)=>{e.target.type = "date"}}
+              onBlur={(e) => {e.target.type = "text"}}
             />
-            <button className="px-4 py-2 bg-blue-500 text-white rounded-md shadow-sm hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
-              Search
-            </button>
-          </div>
-            </div>
-            )}
+            <InputReusable
+              type="text"
+              placeholder="End Date"
+              // value={}
+              // onChange={}
+              onFocus={(e) => {e.target.type = "date"}}
+              onBlur={(e) => {e.target.type = "text"}}
+
+            />
+            <BtnReusable value={"Search"} type={"add"}
+            />
           </div>
         </div>
-      </div>
-    </div>
+      }
+    />
   );
 };
 
--- src/components/ReusableComponents/InputReusable.jsx
+++ src/components/ReusableComponents/InputReusable.jsx
@@ -1,16 +1,27 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-const InputReusable = ({ type, placeholder, value, onChange, onFocus,onBlur, notAllowed }) => {
-
+const InputReusable = ({
+  type,
+  placeholder,
+  value,
+  onChange,
+  onFocus,
+  onBlur,
+  notAllowed,
+}) => {
   return (
-    <input className={`w-full px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-200 dark:text-gray-200 dark:bg-gray-600 ${notAllowed ? "cursor-wait" : "cursor-auto"}`}
+    <input
+      className={`w-full px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-gray-400 dark:placeholder:text-gray-200 dark:text-gray-200 dark:bg-gray-600 ${
+        notAllowed ? "cursor-wait" : "cursor-auto"
+      }`}
       type={type}
       placeholder={placeholder}
       value={value}
       onChange={onChange}
       onFocus={onFocus}
-      onBlur={onBlur} />
-  )
-}
+      onBlur={onBlur}
+    />
+  );
+};
 
 export default InputReusable;
