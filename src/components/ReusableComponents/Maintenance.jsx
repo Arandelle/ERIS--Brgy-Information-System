77be102106ffc388cd0a09e444570710c0ba3a13
Arandelle <topaguintsarandell@gmail.com>
2024-06-03 12:08:26 +0800

    updates

--- src/components/ReusableComponents/Maintenance.jsx
+++ src/components/ReusableComponents/Maintenance.jsx
@@ -6,10 +6,11 @@ const Maintenance = ({ title }) => {
   return (
     <div>
       <div>
-        <h1 className="dark:text-white">This is <span className="text-lg text-primary">{title}</span></h1>
+        <h1 className="text-lg text-primary">{title}</h1>
         <div className="relative flex-col flex items-center justify-center translate-y-1/2">
           <img src={Maintenances} alt="Nothing here" className="h-[250px] w-[250px]" />
-          <h3 className="dark:text-white">Sorry the <span className="text-lg text-primary">{title}</span> is under maintenance</h3>
+          <h3 className="dark:text-white"> <span className="text-lg text-primary">{title}</span> is temporarily unavailable. </h3>
+          <h3 className="dark:text-white">Please check again later. Thank you for your patience!</h3>
         </div>
       </div>
     </div>
--- src/components/Services/Request.jsx
+++ src/components/Services/Request.jsx
@@ -1,31 +1,11 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import Header from '../Header'
-import Sidebar from '../Sidebar/Sidebar'
-import { Toggle } from '../../hooks/Toggle'
-import Skeleton from '../ReusableComponents/Skeleton'
 import Maintenance from '../ReusableComponents/Maintenance'
+import HeadSide from '../ReusableComponents/HeaderSidebar'
 
 const Request = () => {
-    const {isOpen, toggleDropdown}= Toggle();
-    const [loading, setLoading] = useState(true);
     return (
-      <div className='w-full flex-col flex'>
-        <Header toggleSideBar={toggleDropdown}/>
-        <div className='flex'>
-            <div className='fixed z-50'>
-              <Sidebar isOpen={isOpen} toggleSidebar={toggleDropdown}/>
-            </div>
-            <div className={`w-full ${isOpen ? "ml-0" : "md:ml-60"}`}>
-            <div className='m-3'>
-              {loading ? (<Skeleton loading={loading} setLoading={setLoading}/>) :
-                (
-                <Maintenance title={"Request"}/>
-                )}
-            </div>
-            </div>
-        </div>      
-      </div>
+      <HeadSide child={<div className="m-3"><Maintenance title={"News Section"}/>
+      </div>}/>
     )
 }
 
