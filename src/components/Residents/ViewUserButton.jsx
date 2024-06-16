ec926cd3edae20deb3f359326f9666d238e607c0
Arandelle <topaguintsarandell@gmail.com>
2024-06-17 02:19:47 +0800

    Added background color for view selected user, if it is viewing selected user, it turn primary-400

--- src/components/Residents/ViewUserButton.jsx
+++ src/components/Residents/ViewUserButton.jsx
@@ -8,7 +8,7 @@ const ViewUserButton = ({filteredResidents, isViewingSelected, setIsViewingSelec
   return (
     <button
       onClick={toggleViewSelected}
-      className={`inline-flex items-center text-nowrap text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700`}
+      className={`inline-flex items-center text-nowrap border border-gray-300 focus:outline-none focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700 ${isViewingSelected ? "bg-primary-400 hover:bg-primary-500 text-gray-200" : "bg-white text-gray-500 hover:bg-gray-100 "}`}
     >
       {isViewingSelected ? `All users (${filteredResidents.length})` : `Selected (${selectedUsers.length})`}
     </button>
