90ab03e79cb485310cddcecfad7045a803b8346f
Arandelle <topaguintsarandell@gmail.com>
2024-06-20 20:44:58 +0800

    feat: Add button to toggle export and import actions with tooltips
    - Added a button to toggle the visibility of export and import buttons.
    - Implemented Material-UI Tooltip for import button, removing manual styling for hover messages.
    - Removed manual useRef and positioning logic for tooltips, simplifying the code.

--- src/components/Residents/FileHandlerButtons.jsx
+++ src/components/Residents/FileHandlerButtons.jsx
@@ -1,97 +1,74 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import DownloadIcon from "@mui/icons-material/Download";
 import PublishIcon from "@mui/icons-material/Publish";
+import CloseIcon from "@mui/icons-material/Close";
+import ListIcon from "@mui/icons-material/List";
+import Tooltip from "@mui/material/Tooltip"; // This is build in toolTip
 
 const FileHandlerButtons = ({ onClickExport, onClickImport }) => {
-  const [isOpenExportTooltip, setIsOpenExportTooltip] = useState(false);
-  const [isOpenImportTooltip, setIsOpenImportTooltip] = useState(false);
-  const exportButtonRef = useRef(null);
-  const importLabelRef = useRef(null);
-  const exportTooltipRef = useRef(null);
-  const importTooltipRef = useRef(null);
-
-  useEffect(() => {
-    if (isOpenExportTooltip) {
-      positionTooltip(exportButtonRef.current, exportTooltipRef.current);
-    }
-    if (isOpenImportTooltip) {
-      positionTooltip(importLabelRef.current, importTooltipRef.current);
-    }
-  }, [isOpenExportTooltip, isOpenImportTooltip]);
-
-  const positionTooltip = (triggerElement, tooltipElement) => {
-    const triggerRect = triggerElement.getBoundingClientRect();
-    const tooltipRect = tooltipElement.getBoundingClientRect();
-
-    let tooltipTop = triggerRect.top - tooltipRect.height - 8; // 8px above the trigger element
-    let tooltipLeft =
-      triggerRect.left + triggerRect.width / 2 - tooltipRect.width / 2; // Centered horizontally
-
-    // Adjust tooltip position to fit within viewport horizontally
-    if (tooltipLeft < 0) {
-      tooltipLeft = 10; // Minimum distance from left edge
-    } else if (tooltipLeft + tooltipRect.width > window.innerWidth) {
-      tooltipLeft = window.innerWidth - tooltipRect.width - 10; // 10px from right edge
-    }
-
-    tooltipElement.style.top = `${tooltipTop}px`;
-    tooltipElement.style.left = `${tooltipLeft}px`;
+  const [isFileButtonsOpen, setIsFileButtonsOpen] = useState(false);
+  const toggleFileHandler = () => {
+    setIsFileButtonsOpen(!isFileButtonsOpen);
   };
 
   return (
-    <div className="flex flex-row space-x-1 relative">
-      {/* Export button */}
-      <button
-        ref={exportButtonRef}
-        onMouseEnter={() => setIsOpenExportTooltip(true)}
-        onMouseLeave={() => setIsOpenExportTooltip(false)}
-        className="flex items-center justify-center rounded-3xl bg-green-400 w-11 py-1 px-2"
-        onClick={onClickExport}
-      >
-        <DownloadIcon style={{ fontSize: "large", color: "white" }} />
-      </button>
+    <div className="relative flex flex-row space-x-1">
 
-      {/* Export tooltip */}
-      <div
-        ref={exportTooltipRef}
-        className={`absolute z-50 inline-block w-36 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800 ${
-          isOpenExportTooltip ? "opacity-100 visible" : "opacity-0 invisible"
-        }`}
-        style={{ top: "-9999px", left: "-9999px", position: "fixed" }}
-      >
-        <div className="px-3 py-2">
-          <p>Export as Excel</p>
+      {/* This button toggles the export and import buttons */}
+      <Tooltip title="Show File Actions" arrow>
+        <button onClick={toggleFileHandler}>
+          <ListIcon />
+        </button>
+      </Tooltip>
+
+      {/* File handler modal */}
+      {isFileButtonsOpen && (
+        <div className="fixed inset-0 flex items-center justify-center z-50">
+          {/* This area when clicked,the modal will close */}
+          <div 
+            className="bg-gray-500 bg-opacity-75 w-full h-full absolute" 
+            onClick={toggleFileHandler}
+          ></div>
+
+          <div className="relative z-50 p-10 bg-white rounded-md shadow-lg max-w-sm mx-auto">
+            <Tooltip title="Close" placement="top" arrow>
+              <button
+                className="absolute top-2 right-2"
+                onClick={toggleFileHandler}
+              >
+                <CloseIcon style={{ color: "gray" }} />
+              </button>
+            </Tooltip>
+            <div className="flex flex-col space-y-2">
+              
+              {/* Export button */}
+              <Tooltip title="Export to Excel" placement="left" arrow>
+                <button
+                  className="flex items-center justify-center rounded-3xl bg-green-500 py-2 px-3"
+                  onClick={onClickExport}
+                >
+                  <DownloadIcon style={{ fontSize: "large", color: "white" }} />
+                  <span className="text-sm text-gray-200">Export</span>
+                </button>
+              </Tooltip>
+
+              {/* Import button */}
+              <Tooltip title="Import from excel" placement="left" arrow>
+                <label className="cursor-pointer flex items-center justify-center rounded-3xl bg-green-500 py-2 px-3">
+                  <PublishIcon style={{ fontSize: "large", color: "white" }} />
+                  <span className="text-sm text-gray-200">Import</span>
+                  <input
+                    type="file"
+                    className="hidden"
+                    accept=".xlsx, .xls"
+                    onChange={onClickImport}
+                  />
+                </label>
+              </Tooltip>
+            </div>
+          </div>
         </div>
-      </div>
-
-      {/* Import button */}
-      <label
-        ref={importLabelRef}
-        onMouseEnter={() => setIsOpenImportTooltip(true)}
-        onMouseLeave={() => setIsOpenImportTooltip(false)}
-        className="cursor-pointer flex items-center justify-center rounded-3xl bg-primary-400 w-11 py-1 px-2"
-      >
-        <PublishIcon style={{ fontSize: "large", color: "white" }} />
-        <input
-          type="file"
-          className="hidden"
-          accept=".xlsx, .xls"
-          onChange={onClickImport}
-        />
-      </label>
-
-      {/* Import tooltip */}
-      <div
-        ref={importTooltipRef}
-        className={`absolute z-50 inline-block w-36 text-sm text-gray-500 transition-opacity duration-300 bg-white border border-gray-200 rounded-lg shadow-sm dark:text-gray-400 dark:border-gray-600 dark:bg-gray-800 ${
-          isOpenImportTooltip ? "opacity-100 visible" : "opacity-0 invisible"
-        }`}
-        style={{ top: "-9999px", left: "-9999px", position: "fixed" }}
-      >
-        <div className="px-3 py-2">
-          <p>Import Excel</p>
-        </div>
-      </div>
+      )}
     </div>
   );
 };
