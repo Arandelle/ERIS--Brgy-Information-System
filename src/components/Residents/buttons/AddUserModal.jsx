2234b580769d21633b71597395c1b8cf5c01c147
Arandelle <topaguintsarandell@gmail.com>
2024-06-24 20:16:23 +0800

    intalled emailJs Library - created an account to emailJs implement functionality to send an otp emails using emailJs added otp verificaton feature

--- src/components/Residents/buttons/AddUserModal.jsx
+++ src/components/Residents/buttons/AddUserModal.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { toast } from "sonner";
 import InputReusable from "../../ReusableComponents/InputReusable";
+import EmailForm from "../../EmailForm";
 
 const AddUserModal = ({ addUser, setAddUser, next, setNext }) => {
   return (
@@ -38,45 +39,46 @@ const AddUserModal = ({ addUser, setAddUser, next, setNext }) => {
         </div>
       )}{" "}
       {next && (
-        <div className="flex items-center justify-center fixed inset-0 z-50">
-          <div
-            className="fixed h-full w-full bg-gray-600 bg-opacity-50"
-            onClick={() => setNext(false)}
-          ></div>
-          <div className="relative p-10 bg-white rounded-md shadow-md">
-            <button
-              className="absolute top-2 right-2"
-              onClick={() => setNext(false)}
-            >
-              Close
-            </button>
-            <div className="flex flex-col space-y-1">
-              <h2>Add user</h2>
-              <InputReusable type={"number"} className={"border"} placeholder={"Enter Age"} />
-              <InputReusable type={"text"} className={"border"} placeholder={"Enter Address"} />
-              <InputReusable type={"text"} onFocus={(e)=> (e.target.type = "date" )} className={"border"} placeholder={"Enter Bday"}/>
-              <div className="flex flex-row space-x-1 justify-between">
-                <button
-                  className="p-2 text-gray-200 bg-primary-500"
-                  onClick={() => {
-                    setAddUser(true);
-                    setNext(false);
-                  }}
-                >
-                  back
-                </button>
-                <button
-                  className="p-2 text-gray-200 bg-primary-500"
-                  onClick={() => {
-                    setNext(false, toast.success("User Added successfully"));
-                  }}
-                >
-                  Submit
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
+        <EmailForm />
+        // <div className="flex items-center justify-center fixed inset-0 z-50">
+        //   <div
+        //     className="fixed h-full w-full bg-gray-600 bg-opacity-50"
+        //     onClick={() => setNext(false)}
+        //   ></div>
+        //   <div className="relative p-10 bg-white rounded-md shadow-md">
+        //     <button
+        //       className="absolute top-2 right-2"
+        //       onClick={() => setNext(false)}
+        //     >
+        //       Close
+        //     </button>
+        //     <div className="flex flex-col space-y-1">
+        //       <h2>Add user</h2>
+        //       <InputReusable type={"number"} className={"border"} placeholder={"Enter Age"} />
+        //       <InputReusable type={"text"} className={"border"} placeholder={"Enter Address"} />
+        //       <InputReusable type={"text"} onFocus={(e)=> (e.target.type = "date" )} className={"border"} placeholder={"Enter Bday"}/>
+        //       <div className="flex flex-row space-x-1 justify-between">
+        //         <button
+        //           className="p-2 text-gray-200 bg-primary-500"
+        //           onClick={() => {
+        //             setAddUser(true);
+        //             setNext(false);
+        //           }}
+        //         >
+        //           back
+        //         </button>
+        //         <button
+        //           className="p-2 text-gray-200 bg-primary-500"
+        //           onClick={() => {
+        //             setNext(false, toast.success("User Added successfully"));
+        //           }}
+        //         >
+        //           Submit
+        //         </button>
+        //       </div>
+        //     </div>
+        //   </div>
+        // </div>
       )}
     </div>
   );
