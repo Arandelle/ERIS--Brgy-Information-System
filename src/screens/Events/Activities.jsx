9b3b9e0ce6b00ae2bc008d83b9ae0e0f7a1e7200
Arandelle <topaguintsarandell@gmail.com>
2024-10-04 00:33:17 +0800

    Redesign the modal to add new announcement

--- src/screens/Events/Activities.jsx
+++ src/screens/Events/Activities.jsx
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
-import {toast} from "sonner"
+import { toast } from "sonner";
 import InputReusable from "../../components/ReusableComponents/InputReusable";
 import BtnReusable from "../../components/ReusableComponents/BtnReusable";
 import HeadSide from "../../components/ReusableComponents/HeaderSidebar";
 import { onValue, push, ref, serverTimestamp } from "firebase/database";
 import { database, storage } from "../../services/firebaseConfig";
-import {getDownloadURL, ref as storageRef, uploadBytes} from "firebase/storage"
+import {
+  getDownloadURL,
+  ref as storageRef,
+  uploadBytes,
+} from "firebase/storage";
 import Table from "../../components/Table";
+import CloseIcon from "@mui/icons-material/Close";
 
 const Activities = () => {
-
   const [activity, setActivity] = useState([]);
   const [title, setTitle] = useState("");
   const [startDate, setStartDate] = useState("");
@@ -17,59 +21,64 @@ const Activities = () => {
   const [image, setImage] = useState("");
   const [modal, setModal] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     const announcementRef = ref(database, `announcement`);
     const unsubscribe = onValue(announcementRef, (snapshot) => {
-      try{
+      try {
         const announcementData = snapshot.val();
         const announcementList = Object.keys(announcementData).map((key) => ({
           id: key,
-          ...announcementData[key]
+          ...announcementData[key],
         }));
-        setActivity(announcementList)
-      } catch(error){
-        console.log("Error: ", error)
+        setActivity(announcementList);
+      } catch (error) {
+        console.log("Error: ", error);
       }
     });
 
-    return ()=> unsubscribe()
+    return () => unsubscribe();
   }, []);
 
   const handleModal = () => {
     setModal(!modal);
-  }
+  };
 
-  const handleImageChange = (e) =>{
-    const file = e.target.files[0]
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
 
-    if(file){
-      setImage(file)
+    if (file) {
+      setImage(file);
     }
-  }
+  };
 
   const handleAddAnnouncement = async () => {
     if (!title || !startDate || !description) {
-      toast.info("Please complete the form")
+      toast.info("Please complete the form");
       return;
     }
-    if(new Date(startDate).setHours(0,0,0,0) < new Date().setHours(0,0,0,0)){
+    if (
+      new Date(startDate).setHours(0, 0, 0, 0) < new Date().setHours(0, 0, 0, 0)
+    ) {
       toast.error("Start date must be greater than or equal to today's date");
       return;
     }
 
-    let imageUrl = ""
+    let imageUrl = "";
 
-    if(image){
-      const imageFile = image
-      const imageRef = storageRef(storage, `announcement-images/${imageFile.name}`);
+    if (image) {
+      const imageFile = image;
+      const imageRef = storageRef(
+        storage,
+        `announcement-images/${imageFile.name}`
+      );
 
-      try{
+      try {
         // upload image to firabase storage
-        await uploadBytes(imageRef, imageFile)
+        await uploadBytes(imageRef, imageFile);
         imageUrl = await getDownloadURL(imageRef);
-      } catch(error){
+      } catch (error) {
         console.error("Error uploading image: ", error);
-        toast.error(`Error uploading image: ${error}`)
+        toast.error(`Error uploading image: ${error}`);
         return;
       }
     }
@@ -85,106 +94,122 @@ const Activities = () => {
 
     const announcementRef = ref(database, `announcement`);
 
-    try{
+    try {
       await push(announcementRef, addedNews);
-      toast.success("Submmited successfully!")
-    }
-    catch(error){
-      console.error("Error :", error)
+      toast.success("Submmited successfully!");
+    } catch (error) {
+      console.error("Error :", error);
     }
 
     setTitle("");
     setStartDate("");
     setDescription("");
-    setImage("")
+    setImage("");
   };
 
-  const headerData = [
-    "Title",
-    "Date",
-    "Description",
-    "Image",
-    "Action"
-  ]
+  const headerData = ["Title", "Date", "Description", "Image", "Action"];
 
   const renderRow = (announcement) => {
-    return(
+    return (
       <>
-      <td className="px-6 py-4">{announcement.title}</td>
-       <td className="px-6 py-4">{announcement.startDate}</td>
-       <td className="px-6 py-4 whitespace-nowrap">{announcement.description}</td>
-       <td className="px-6 py-4">
-       <img src= {announcement.imageUrl} alt="image" className="h-24 w-24 rounded-full" />
-      </td>
-      <td>
-       <div className="flex flex-row items-center justify-evenly">
-          <BtnReusable
-          value={"Delete"}
-          type="delete"
+        <td className="px-6 py-4">{announcement.title}</td>
+        <td className="px-6 py-4">{announcement.startDate}</td>
+        <td className="px-6 py-4 whitespace-nowrap">
+          {announcement.description}
+        </td>
+        <td className="px-6 py-4">
+          <img
+            src={announcement.imageUrl}
+            alt="image"
+            className="h-24 w-24 rounded-full"
           />
-           <BtnReusable
-          value={"Edit"}
-          type="edit"
-          />
-       </div>
-      </td>
-     </>
-    )
-  }
+        </td>
+        <td>
+          <div className="flex flex-row items-center justify-evenly">
+            <BtnReusable value={"Delete"} type="delete" />
+            <BtnReusable value={"Edit"} type="edit" />
+          </div>
+        </td>
+      </>
+    );
+  };
   return (
-    <HeadSide child={  
-    <div className="m-3">
-    <div className="flex flex-col gap-4">
-      <BtnReusable 
-        value={"Add announcement"}
-        type="add"
-      />
-      <div className="grid justify-start grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-15 my-15 gap-4">
-        <InputReusable
-          type="text"
-          value={title}
-          placeholder="Activity Title"
-          onChange={(e) => setTitle(e.target.value)}
-          onBlur={() => setTitle(title.toUpperCase())}
-        />
-         <InputReusable
-          type="text"
-          onFocus={(e) => (e.target.type = "date")}
-          onBlur={(e) => (e.target.type = "text")}
-          value={startDate}
-          placeholder="Date"
-          onChange={(e) => setStartDate(e.target.value)}
-        />
-      </div>
-      <div>
-        <textarea
-          className={`w-full relative z-1 px-4 py-2 border-gray-300 rounded-md focus:outline-none focus:ring-gray-400 dark:placeholder:text-gray-200 dark:text-gray-200 dark:bg-gray-600
-          }`}
-          value={description}
-          placeholder="Description"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-       <div className="flex flex-row">
-          <InputReusable
-          type="file"
-          onChange={handleImageChange}
+    <HeadSide
+      child={
+        <div className="m-3">
+          <div className="pb-3">
+            <BtnReusable
+              value={"Add announcement"}
+              type="add"
+              onClick={handleModal}
+            />
+          </div>
+          <Table
+            headers={headerData}
+            data={activity}
+            renderRow={renderRow}
+            emptyMessage="No announcement found"
           />
-          <BtnReusable
-          value={"Submit"}
-          type={"add"}
-          onClick={handleAddAnnouncement}
-          className={"w-60"}
-          />
-       </div>
-      </div>
-    </div>
-      <Table 
-        headers={headerData}
-        data={activity}
-        renderRow={renderRow}
-        emptyMessage="No announcement found"
-      />
-  </div>} />
+
+          {modal && (
+            <div className="fixed flex items-center justify-center inset-0 z-50">
+              <div
+                className="fixed h-full w-full bg-gray-600 bg-opacity-50"
+                onClick={handleModal}
+              ></div>
+              <div className="relative p-5 bg-white rounded-md shadow-md">
+                <h2 className="py-2 px-2 text-primary-500 border-2 mb-5">
+                  Add new announcement
+                </h2>
+                <button
+                  className="absolute p-2 top-0 right-0"
+                  onClick={handleModal}
+                >
+                  <CloseIcon style={{ fontSize: "large" }} />
+                </button>
+                <div className="w-full flex flex-col">
+                  <div className="flex flex-row space-x-3 py-2">
+                      <InputReusable
+                        type="text"
+                        placeholder="Title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                        onBlur={() => setTitle(title.toUpperCase())}
+                        className={"w-full"}
+                      />
+                      <InputReusable
+                        type="text"
+                        onFocus={(e) => (e.target.type = "date")}
+                        onBlur={(e) => (e.target.type = "text")}
+                        placeholder="Start Date"
+                        value={startDate}
+                        onChange={(e) => setStartDate(e.target.value)}
+                        className={"w-full"}
+                      />
+                  </div>
+                  <textarea
+                        className={`border-gray-300 rounded-md  dark:placeholder:text-gray-200 dark:text-gray-200 dark:bg-gray-600
+            }`}
+                        value={description}
+                        placeholder="Description"
+                        onChange={(e) => setDescription(e.target.value)}
+                      />
+                    <div className="flex flex-row py-2">
+                        <InputReusable type="file" onChange={handleImageChange} />
+                        <BtnReusable
+                          value={"Submit"}
+                          type={"add"}
+                          onClick={handleAddAnnouncement}
+                          className={"w-60"}
+                        />
+                    </div>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      }
+    />
   );
 };
 
