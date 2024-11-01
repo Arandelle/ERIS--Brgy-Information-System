57fb128c99573728da3fa939d6a20d0b2f5b4a9e
Arandelle <topaguintsarandell@gmail.com>
2024-11-01 19:41:44 +0800

    Refactor the alignment of image and value for dashboaroad card, changed the today regitered to on-going emergency

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -6,10 +6,12 @@ import population from "../assets/images/population.svg";
 import registered from "../assets/images/registered.svg";
 import Events from "../assets/images/events.svg";
 import emergency from "../assets/images/emergency.svg";
+import navigator from "../assets/images/direction.svg"
 import HeadSide from "../components/ReusableComponents/HeaderSidebar";
 import DateToday from "../helper/DateToday";
 import { useFetchData } from "../hooks/useFetchData";
 import icons from "../assets/icons/Icons";
+import { Tooltip } from "@mui/material";
 
 const DashboardCard = ({
   title,
@@ -114,13 +116,34 @@ const DashboardCard = ({
     }
   };
 
+  const tooltipTitle = () => {
+
+    switch(selectedOption){
+      case "daily":
+        return "Compared to yesterday";
+      case "previousDay":
+        return "Compared to today";
+      case "weekly":
+        return "Compared to last week";
+      case "previousWeek":
+        return "Compared to this week";
+      case "monthly":
+        return "Compared to last month";
+      case "previousMonth":
+        return "Compared to this month";
+      default:
+        return "Compared to yesterday's data";
+    }
+  }
+
+  const titleToolTip = tooltipTitle()
   const displayValue = getDisplayValue();
 
   return (
     <div className="relative">
       <div className="flex h-full flex-col bg-white dark:bg-gray-800 shadow-md rounded-md p-4 w-full mb-3 md:mb-0">
         <div className="flex justify-between items-center">
-          <p className="text-xs font-bold uppercase text-gray-600 dark:text-gray-400">
+          <p className="text-xs text-wrap whitespace-normal w-20 font-bold uppercase text-gray-600 dark:text-gray-400">
             {title}
           </p>
           {hasOption && (
@@ -128,7 +151,7 @@ const DashboardCard = ({
               id="select"
               value={selectedOption}
               onChange={(e) => onOptionChange(e.target.value)}
-              className="text-sm font-semibold text-gray-500 cursor-pointer border-b-1 border-t-0 border-x-0 focus:ring-0"
+              className="text-sm dark:bg-gray-800 dark:text-gray-400 font-semibold text-gray-500 cursor-pointer border-b-1 border-t-0 border-x-0 focus:ring-0"
             >
               <option value="daily">Today</option>
               <option value="previousDay">Yesterday</option>
@@ -140,17 +163,18 @@ const DashboardCard = ({
           )}
         </div>
 
-        <div className="flex justify-between items-center mt-4">
+        <div className="flex items-center pt-4 px-2">
           <div className="w-full hidden md:block">
             <img
               src={img}
               alt="Icon"
               onClick={onClick}
-              className="h-16 w-16 cursor-pointer"
+              className="h-20 w-20 cursor-pointer"
             />
           </div>
 
-          <div className="flex flex-row md:flex-col justify-between w-full items-center md:items-end">
+          <div className={`flex flex-row md:flex-col justify-between w-full
+          ${(hasOption || title === "Today's Emergency") ? "items-end" : "items-center"}`}>
             <p
               onClick={onClick}
               className="text-2xl cursor-pointer text-center font-bold text-primary-500 dark:text-primary-400"
@@ -158,8 +182,8 @@ const DashboardCard = ({
               {displayValue}
             </p>
             <p
-              className={`text-lg md:text-sm font-semibold text-nowrap ${
-                title === "Total Responded"
+              className={`text-lg md:text-sm font-semibold cursor-pointer ${
+                title === "Total Responses"
                   ? isIncrease
                     ? "text-green-500"
                     : "text-red-500"
@@ -170,10 +194,12 @@ const DashboardCard = ({
                   : ""
               }`}
             >
-              {(title === "Total Responded" || title === "Today's Emergency") &&
-                (percentage !== 0
-                  ? `${Math.abs(percentage)}% ${isIncrease ? "↑" : "↓"}`
-                  : "0%")}
+             <Tooltip title={titleToolTip} placement="top" arrow>
+                {(title === "Total Responses" || title === "Today's Emergency") ?
+                  (percentage !== 0
+                    ? `${Math.abs(percentage)}% ${isIncrease ? "↑" : "↓"}`
+                    : "0%") : (<span className="invisible">extra space</span>)}
+             </Tooltip>
             </p>
           </div>
         </div>
@@ -185,13 +211,13 @@ const DashboardCard = ({
 const Dashboard = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
-  const { data: users } = useFetchData("users");
   const { data: emergencyData } = useFetchData("emergencyRequest");
   const { data: events } = useFetchData("events");
   const { data: announcement } = useFetchData("announcement");
-  const [selectedOption, setSelectedOption] = useState("daily");
+  const [selectedOption, setSelectedOption] = useState("monthly");
 
   const activities = [...events, ...announcement];
+  const onGoing = emergencyData.filter((item) => item.status === "on-going")
 
   const calculateCounts = (emergencyData) => {
     const now = new Date();
@@ -284,7 +310,7 @@ const Dashboard = () => {
 
           <div className="grid sm:grid-cols-1 gap-0 md:grid-cols-2 md:gap-4 md:w-max-40 xl:grid-cols-4 md:my-3 text-wrap space-y-2 md:space-y-0">
             <DashboardCard
-              title="Total Responded"
+              title="Total Responses"
               value={
                 loading ? (
                   <Spinner setLoading={setLoading} />
@@ -314,10 +340,10 @@ const Dashboard = () => {
               counts={counts.awaitingResponse}
             />
             <DashboardCard
-              title="Today's Register"
-              value={loading ? <Spinner setLoading={setLoading} /> : 0}
-              img={registered}
-              onClick={() => handleNavigate("/accounts/users")}
+              title="On-Going Emergency"
+              value={loading ? <Spinner setLoading={setLoading} /> : onGoing.length}
+              img={navigator}
+              onClick={() => handleNavigate("/maps")}
               hasOption={false}
             />
             <DashboardCard
