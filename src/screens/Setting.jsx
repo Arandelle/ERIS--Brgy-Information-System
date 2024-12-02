205bbee02740ce95075655fb566f7a5ee2d496ce
Arandelle Paguinto <topaguintsarandell@gmail.com>
2024-12-02 22:26:38 +0800

    Added preview image, created input for image and hide it

--- src/screens/Setting.jsx
+++ src/screens/Setting.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import HeadSide from "../components/ReusableComponents/HeaderSidebar";
-import Logo from "../assets/images/logo.png"
-import Iconbutton from "../components/ReusableComponents/IconButton"
+import Logo from "../assets/images/logo.png";
+import Iconbutton from "../components/ReusableComponents/IconButton";
 import icons from "../assets/icons/Icons";
 import { auth } from "../services/firebaseConfig";
 import { useFetchData } from "../hooks/useFetchData";
 
 const Setting = () => {
-  const [title, setTitle] = useState("");
-  const [logo, setLogo] = useState("");
+  const [title, setTitle] = useState("Bagtas");
+  const [image, setImage] = useState("");
+  const [preview, setPreview] = useState("");
   const user = auth.currentUser;
-  const {data: admin} = useFetchData("admins");
+  const { data: admin } = useFetchData("admins");
   const currentAdminDetails = admin.find((admin) => admin.id === user.uid);
 
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file && file.type.startsWith("image/") && file.size <= 5 * 1024 * 1024) {
+      setImage(file);
+      setPreview(URL.createObjectURL(file));
+    } else {
+      toast.error("Invalid file type or size. Please upload an image under 5MB.");
+    }
+    
+  };
+
+
+  console.log(image)
   return (
     <HeadSide
       child={
@@ -36,16 +51,19 @@ const Setting = () => {
                   </div>
                   <div className="flex flex-col">
                     <p className="font-medium text-lg">
-                      {currentAdminDetails?.firstname} {currentAdminDetails?.lastname}
+                      {currentAdminDetails?.firstname}{" "}
+                      {currentAdminDetails?.lastname}
                     </p>
-                    <p className="text-gray-500 text-sm">
-                     {user.email}
-                    </p>
+                    <p className="text-gray-500 text-sm">{user.email}</p>
                   </div>
                 </div>
                 <button className="bg-gray-100 font-medium text-sm border py-1 px-2 rounded-md flex flex-row space-x-4 items-center">
                   Edit Profile
-                  <Iconbutton icon={icons.edit} color={"gray"} fontSize={"small"} />
+                  <Iconbutton
+                    icon={icons.edit}
+                    color={"gray"}
+                    fontSize={"small"}
+                  />
                 </button>
               </div>
             </div>
@@ -57,9 +75,7 @@ const Setting = () => {
               </p>
               <div className="space-y-3 py-6 px-8">
                 <div className="flex flex-row items-center justify-between w-3/4">
-                  <p className="font-medium text-gray-800 text-lg">
-                    Title
-                  </p>
+                  <p className="font-medium text-gray-800 text-lg">Title</p>
                   <input
                     type="text"
                     disabled
@@ -67,14 +83,16 @@ const Setting = () => {
                     className="rounded-lg shadow-sm border-2 border-gray-200 bg-gray-200 text-gray-800 text-sm"
                   />
                   <button className="bg-gray-100 font-medium text-sm py-1 px-2 border rounded-lg flex flex-row items-center">
-                  Edit Title
-                  <Iconbutton icon={icons.edit} color={"gray"} fontSize={"small"} />
+                    Edit Title
+                    <Iconbutton
+                      icon={icons.edit}
+                      color={"gray"}
+                      fontSize={"small"}
+                    />
                   </button>
                 </div>
                 <div className="flex flex-row items-center justify-between w-3/4">
-                  <p className="font-medium text-gray-800 text-lg">
-                    Barangay
-                  </p>
+                  <p className="font-medium text-gray-800 text-lg">Barangay</p>
                   <select className="rounded-lg shadow-sm border-2 border-gray-200 text-sm">
                     <option>Bagtas</option>
                   </select>
@@ -83,16 +101,22 @@ const Setting = () => {
               </div>
 
               <div className="flex flex-row items-center  w-3/4">
-                <img src={Logo} className="w-40 rounded-full" />
-                <button className="bg-gray-100 font-medium text-sm p-2 border rounded-lg">
+                <img src={preview || Logo} className="w-40 rounded-full" />
+                <label
+                  for="file-upload"
+                  class="bg-gray-100 font-medium text-sm whitespace-nowrap p-2 border rounded-lg cursor-pointer"
+                >
                   Upload Photo
-                </button>
+                  <input id="file-upload" type="file" class="hidden" onChange={handleImageChange} />
+                </label>
               </div>
             </div>
 
             {/**Save button */}
             <div className="py-4 place-self-end">
-              <button className="bg-blue-500 py-2 px-4 rounded-md text-sm text-white">Save update</button>
+              <button className="bg-blue-500 py-2 px-4 rounded-md text-sm text-white">
+                Save update
+              </button>
             </div>
           </div>
         </div>
