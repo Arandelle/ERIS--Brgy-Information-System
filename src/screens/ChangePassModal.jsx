0650ed817f4741e0f4e7ed6b3b9190e3f9108f98
Arandelle Paguinto <topaguintsarandell@gmail.com>
2024-12-20 22:04:52 +0800

    Update the changing password: user emailauthprovider to get the credentials of current user user reauthenticateWithCredential to check if the credentials is correct, if correct proceeed to check the new password andre enter password

--- src/screens/ChangePassModal.jsx
+++ src/screens/ChangePassModal.jsx
@@ -1,27 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { ProfileModalStyle } from "./ProfileModal";
 import { InputField } from "../components/ReusableComponents/InputField";
+import { auth } from "../services/firebaseConfig";
+import { EmailAuthProvider, reauthenticateWithCredential, updatePassword } from "firebase/auth";
+import { toast } from "sonner";
 
 const ChangePassModal = () => {
+  const user = auth.currentUser;
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [reEnterPassword, setReEnterPassword] = useState("");
+
+  const handleChangePassword = () => {
+    if (user) {
+      const newPass = newPassword;
+
+      const credential = EmailAuthProvider.credential(user.email, oldPassword);
+    
+      reauthenticateWithCredential(user, credential).then(() => {
+        if(newPassword === reEnterPassword){
+        updatePassword(user, newPass)
+        .then(() => {
+          toast.success("Successfully update the password!");
+        })
+        .catch((error) => {
+          toast.error(`Error updating password ${error}`);
+        });
+        } else{
+            toast.error("New password do not match");
+        }
+      }).catch((error) => {
+        toast.error(`Error re-authenticating: ${error.message}`)
+      })
+      
+    } else {
+      toast.error("No user is logged in");
+    }
+  };
   return (
     <ProfileModalStyle
       inputsArea={
         <>
           <InputField
-            // value={adminData.fullname}
             type="password"
             placeholder={"Enter old password"}
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
           />
-          <InputField type="password" placeholder={"Enter new password"} />
+          <InputField
+            type="password"
+            placeholder={"Enter new password"}
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
           <InputField
             type="password"
             placeholder={"Re-enter your new password"}
+            value={reEnterPassword}
+            onChange={(e) => setReEnterPassword(e.target.value)}
           />
         </>
       }
       submitButton={
         <button
           className="bg-green-500 w-full py-2 text-white text-sm rounded-md"
+          onClick={handleChangePassword}
         >
           Change Password
         </button>
