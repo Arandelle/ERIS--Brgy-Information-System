9851131e544c09f30e934beceb9edb3462d58ca4
Arandelle Paguinto <topaguintsarandell@gmail.com>
2024-12-21 13:19:35 +0800

    Refactor the announcement details modal: creating reusable component and handle some conditon if the value is image or links

--- src/screens/Announcement/DetailsAnnouncement.jsx
+++ src/screens/Announcement/DetailsAnnouncement.jsx
@@ -1,69 +1,97 @@
 import Modal from "../../components/ReusableComponents/Modal";
 import { useFetchData } from "../../hooks/useFetchData";
-import { formatDateWithTime } from "../../helper/FormatDate";
 import ButtonStyle from "../../components/ReusableComponents/Button";
 import icons from "../../assets/icons/Icons";
 import { getTimeDifference } from "../../helper/TimeDiff";
+import useImageView from "../../hooks/useImageView";
+import ViewImage from "../ViewImage";
+import { formatDateWithTime } from "../../helper/FormatDate";
 
-const DetailsAnnouncement = ({ closeButton, selectedId, handleEditClick, handleDeleteClick }) => {
-  const {data: activity} = useFetchData("announcement");
+const DetailsAnnouncement = ({
+  closeButton,
+  selectedId,
+  handleEditClick,
+  handleDeleteClick,
+}) => {
+  const { isModalOpen, currentImage, openModal, closeModal } = useImageView();
+  const { data: activity } = useFetchData("announcement");
   const announcementDetails = activity.find(
     (announcement) => announcement.id === selectedId
   );
+
+  const DetailRow = ({ label, value, isLink, isImage }) => {
+    return (
+      <div className="flex flex-row max-w-lg">
+        <p className="flex-1 basis-1/4">{label} :</p>
+
+        {isLink ? (
+          <a className="flex-1 basis-3/4 text-blue-500" href={value}>
+            {announcementDetails?.title} <icons.view />
+          </a>
+        ) : isImage ? (
+          <p
+            className="flex-1 basis-3/4 cursor-pointer bg-gray-100 p-2 rounded-md shadow-md"
+            onClick={() => openModal(announcementDetails?.imageUrl)}
+          >
+            {`${announcementDetails?.title}.jpg`}
+          </p>
+        ) : (
+          <p className="flex-1 basis-3/4">{value}</p>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <Modal closeButton={closeButton}>
-      <div className="flex flex-col items-center justify-center space-y-4">
-        <div className="rounded-full border-2 border-blue-500 p-0.5">
-          <img
-            src={announcementDetails?.imageUrl}
-            alt="Event"
-            className="rounded-full h-20 w-20 lg:h-24 lg:w-24"
+    <>
+      <Modal closeButton={closeButton} title={"Post Details"}>
+        <DetailRow
+          label={"Image"}
+          value={announcementDetails?.description}
+          isImage
+        />
+        <DetailRow
+          label={"Description"}
+          value={announcementDetails?.description}
+        />
+        <DetailRow
+          label={"Date"}
+          value={formatDateWithTime(announcementDetails?.date)}
+        />
+        <DetailRow label={"Links"} value={announcementDetails?.links} isLink />
+  
+        <DetailRow
+          label={"Last Edit"}
+          value={getTimeDifference(announcementDetails?.timestamp)}
+        />
+        <div className="space-x-4">
+          <ButtonStyle
+            label="Edit Post"
+            icon={icons.edit}
+            color={"green"}
+            fontSize={"small"}
+            onClick={() => {
+              handleEditClick(announcementDetails);
+            }}
+          />
+          <ButtonStyle
+            icon={icons.delete}
+            fontSize={"small"}
+            label={"Delete Post"}
+            color={"red"}
+            onClick={() => {
+              handleDeleteClick(announcementDetails.id);
+            }}
           />
         </div>
-        <div className="text-sm md:text-md">
-          <p className="font-bold text-center">{announcementDetails?.title}</p>
-          <p className="text-gray-500 text-center">
-            {formatDateWithTime(announcementDetails?.date)}
-          </p>
-        </div>
-      </div>
-
-      <div className="flex flex-row">
-        <p className="w-1/3 text-gray-500">Description: </p>
-        <p className="flex-1 font-bold">{announcementDetails?.description}</p>
-      </div>
-      <div className="flex flex-row">
-        <p className="w-1/3 text-gray-500">Links: </p>
-        <a 
-        className="flex-1 font-light w-1/6 overflow-hidden text-ellipsis text-blue-500"
-        href={announcementDetails?.links}>{announcementDetails?.title} <icons.view /></a>
-      </div>
-      <div className="flex flex-row">
-        <p className="w-1/3 text-gray-500">Last Edit: </p>
-        <p className="flex-1 font-bold">{getTimeDifference(announcementDetails?.timestamp)}</p>
-      </div>
-
-      <div className="space-x-4">
-        <ButtonStyle
-          label="Edit Post"
-          icon={icons.edit}
-          color={"green"}
-          fontSize={"small"}
-          onClick={() => {
-            handleEditClick(announcementDetails);
-          }}
+      </Modal>
+      {isModalOpen && (
+        <ViewImage 
+          currentImage={currentImage}
+          closeModal={closeModal}
         />
-        <ButtonStyle
-          icon={icons.delete}
-          fontSize={"small"}
-          label={"Delete Post"}
-          color={"red"}
-          onClick={() => {
-            handleDeleteClick(announcementDetails.id);
-          }}
-        />
-      </div>
-    </Modal>
+      )}
+    </>
   );
 };
 
