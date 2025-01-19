63391fa87ea3c287a1913119d9b5e8968211de09
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-01-20 01:18:20 +0800

    Refactor the view user modal design

--- src/screens/AccountList/ViewUserModal.jsx
+++ src/screens/AccountList/ViewUserModal.jsx
@@ -4,80 +4,46 @@ import {formatDateWithTime } from "../../helper/FormatDate";
 
 
 const ViewUserModal = ({ setViewUser, userToViewInfo }) => {
+
+  const {fullname, age, gender, address, email,mobileNum, customId, createdAt, profileComplete} = userToViewInfo;
+  const FetchDataStyle = ({label, data}) => {
+    return (
+      <div className="flex flex-row">
+        <p className="flex-1 basis-1/3">{label} : </p>
+        <p className="flex-1 basis-2/3 font-semibold">{data}</p>
+      </div>
+    );
+  };
+
   return (
     <Modal
       closeButton={() => setViewUser(!true)}
       title={"User Profile"}
       children={
         <>
-          <div className="flex flex-col justify-between space-y-2 min-w-[20rem] lg:min-w-[32rem]">
-            <p className="flex items-center justify-center p-2">
-              <img
-                src={userToViewInfo.img}
-                alt="Profile"
-                className="h-24 w-24"
-              />
-              
-            </p>
-            <div className="flex flex-col space-y-2">
-              <div className="space-y-2">
-                <p className="flex flex-row items-center justify-center space-x-2 font-bold text-gray-500 text-lg border-b-2 border-b-gray-300">
-                  <p>
-                    {userToViewInfo?.firstname && userToViewInfo?.lastname
-                      ? `${userToViewInfo.firstname} ${userToViewInfo.lastname}`
-                      : "Anonymous"}
-                  </p>
-                  {userToViewInfo.gender === "male" ? (
-                    <icons.male className="text-gray-500" />
-                  ) : (
-                    <icons.female className="text-gray-500" />
-                  )}
-                </p>
-                <div className="text-center text-gray-500 flex flex-row justify-evenly">
-                  <p className="italic text-gray-900">
-                    {userToViewInfo.email ?? "email not set"}
-                  </p>
-                  <p className="italic text-gray-900">
-                    {userToViewInfo.mobileNum
-                      ? userToViewInfo.mobileNum
-                      : "mobile number"}
-                  </p>
-                </div>
-                <p className="text-center flex flex-row justify-evenly bg-gray-200 p-2 text-sm text-gray-900 font-thin lowercase italic">
-                  <p className="font-bold">user id:</p>{" "}
-                  {userToViewInfo.customId}
-                </p>
-
-                <div className="bg-gray-100 p-4 rounded-sm">
-                  <p className="text-gray-900 text-lg">
-                    {userToViewInfo.address ?? "address not set"}
-                  </p>
-                  <p className="text-gray-900 text-lg">
-                    {formatDateWithTime(userToViewInfo.createdAt) ?? "address not set"}
-                  </p>
-                </div>
-              </div>
-              <p className="text-center text-lg uppercase">
-                {userToViewInfo?.profileComplete ? (
-                  <p className="text-green-500 font-bold bg-green-100 p-2">
-                    completed
-                  </p>
-                ) : (
-                  <p className="text-red-500 font-bold bg-red-100">
-                    not completed
-                  </p>
-                )}
-              </p>
+          <div className="border-b-2 py-4 max-w-lg w-[32rem]">
+            <div className="space-x-4 flex flex-row items-center ">
+             <div className="rounded-full border-2 border-gray-500"> 
+             <img src={userToViewInfo.img} className="rounded-full h-24 w-24"/>
+             </div>
+               <div>
+                  <p className="font-bold text-lg">{fullname || "User's full name"}</p>
+                  <p className="text-sm font-thin text-gray-500">{email || "User's email"}</p>
+                  <p className="text-xs font-thin text-gray-500">{customId || "user's id"}</p>
+               </div>
             </div>
-            <button
-              className="p-2 text-gray-200 bg-primary-500"
-              onClick={() => {
-                setViewUser(false);
-              }}
-            >
-              OK
-            </button>
           </div>
+          <div>
+          <FetchDataStyle label="Phone number" data={mobileNum || "user's phone number"} />
+          <FetchDataStyle label="Address" data={address || "user's address"} />
+          <FetchDataStyle label="Age" data={age || "user's age"} />
+          <FetchDataStyle label="Gender" data={gender || "user's gender"} />
+          <FetchDataStyle label="Created on" data={formatDateWithTime(createdAt)} />
+          {profileComplete && (
+            <p className="text-red-500">Not yet completed</p>
+          )}
+          </div>
+          <div className="place-self-end"><button className="px-4 py-2 rounded-md bg-blue-600 text-white" onClick={() =>  setViewUser(false)}>Ok</button></div>
         </>
       }
     />
