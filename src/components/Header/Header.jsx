e47faafbde5904ba5d67852158a9831451585d99
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-01-28 19:58:33 +0800

    Add localstorage to cached the systemData and prevent the unnessessarry fetching data, and to improve the user experience

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,6 +11,12 @@ import { useNavigate } from "react-router-dom";
 const Header = ({ toggleSideBar, isOpen }) => {
   const navigate = useNavigate();
   const {systemData} = useFetchSystemData();
+
+  const [system, setSystem] = useState(() => {
+    const stored = localStorage.getItem("system");
+    return stored ? JSON.parse(stored) : {};
+  });
+
   const [theme, toggleTheme] = CustomTheme();
   const [isMdScreen, setIsMdScreen] = useState(window.innerWidth >= 768);
 
@@ -25,6 +31,16 @@ const Header = ({ toggleSideBar, isOpen }) => {
     };
   }, []);
 
+  // store the system data to localstorage to cache the data
+  useEffect(() => {
+    if(systemData){
+      const {imageUrl, title} = systemData;
+      const updatedSystem = {logo: imageUrl, title};
+      setSystem(updatedSystem);
+      localStorage.setItem("system", JSON.stringify(updatedSystem));
+    }
+  }, [systemData]);
+
   return (
     <div className="sticky top-0 z-50 px-4 lg:px-6 w-full py-2.5 border-b border-t border-gray-300 dark:border-gray-600 dark:bg-gray-800 bg-white">
       <div className="flex justify-between items-center">
@@ -32,13 +48,7 @@ const Header = ({ toggleSideBar, isOpen }) => {
           <Tooltip
             title={
               <span className="text-sm">
-                {isMdScreen
-                  ? isOpen
-                    ? "Open Sidebar"
-                    : "Close Sidebar"
-                  : isOpen
-                  ? "Close Sidebar"
-                  : "Open Sidebar"}
+                Toggle Sidebar
               </span>
             }
             placement="right"
@@ -71,14 +81,18 @@ const Header = ({ toggleSideBar, isOpen }) => {
               // href="https://eris-brgy-information-system.vercel.app/dashboard"
               onClick={() => navigate("/dashboard")}
               className="flex mr-4 cursor-pointer"
-            >
+            > 
+            {system ? (
               <img
-                src={systemData?.imageUrl}
+                src={system.logo}
                 className="mr-3 h-10 lg:h-12 p-0 hidden md:block"
                 alt="Your Logo"
               />
+            ) : (
+              <div className="w-10 h-10 lg:h-12 bg-gray-300 animate-pulse rounded"></div>
+            )}
               <span className="self-center text-md lg:text-lg font-semibold text-gray-800 dark:text-gray-300">
-               {systemData?.title}
+               {system?.title}
               </span>
             </a>
         </div>
