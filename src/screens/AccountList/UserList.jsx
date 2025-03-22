057674c7f93d97553313657aa8e88cf1fb8e5e5e
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-03-22 18:30:29 +0800

    update backend

--- src/screens/AccountList/UserList.jsx
+++ src/screens/AccountList/UserList.jsx
@@ -23,7 +23,7 @@ import { auth } from "../../services/firebaseConfig";
 import axios from "axios";
 
 const UserList = ({ data }) => {
-  const API_URL = ' https://eris-brgy-information-system-7dpkl5ors.vercel.app ';
+  const API_URL = 'https://eris-backend-9dtiiixk8-arandelle-paguintos-projects.vercel.app ';
   const { searchParams, setSearchParams } = useSearchParam();
   const userId = searchParams.get("uid");
   const { data: userData = [] } = useFetchData(data);
