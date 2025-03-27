ad2b3657802739b6181bca00130047bfb84f7599
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-03-27 11:18:41 +0800

    memoize the updatedData to prevent the unneccessary rerendering

--- src/screens/Audit.jsx
+++ src/screens/Audit.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo} from "react";
 import HeaderAndSideBar from "../components/ReusableComponents/HeaderSidebar";
 import Toolbar from "../components/ToolBar";
 import { useState } from "react";
@@ -22,76 +22,118 @@ const Audit = () => {
   const { data: template = [] } = useFetchData("templateContent");
   const [searchQuery, setSearchQuery] = useState("");
   const [filterData, setFilterData] = useState("");
-  const [actionLog, setActionLog] = useState([]);
 
-  const HeaderData = ["Executor Id","Fullname","Account Type", "Execute Date","Action", "Target ID"];
-  const searchFields = ["date","fullname", "type", "userId","targetId", "action","email"];
+  const HeaderData = [
+    "Executor Id",
+    "Fullname",
+    "Account Type",
+    "Execute Date",
+    "Action",
+    "Target ID",
+  ];
+  const searchFields = [
+    "date",
+    "fullname",
+    "type",
+    "userId",
+    "targetId",
+    "action",
+    "email",
+  ];
 
   // removes data in 7 days
   useRemoveOldData("usersLog");
 
-  useEffect(() => {
-    if(!userlogs && userlogs.length === 0) return;
+  // memoize action list
+  const actionLog = useMemo(() => {
+    if(!userlogs || userlogs.length === 0) return [];
 
-  // Extract unique actions from user logs, then sort them alphabetically
-    const actions = [...new Set(userlogs.map((log) => log.action).sort((a,b) => a.localeCompare(b)))];
-    setActionLog(actions);
-  }, [userlogs  ]);
+    return [...new Set(userlogs.map((log) => log.action).sort((a,b) => a.localeCompare(b)))];
+
+  }, [userlogs]);
 
   // updated data to include the name of users and responder which not included in original list of emergencyHistory
-  const updatedData = userlogs.map((log) => {
+  const updatedData = useMemo(() => {
+    return userlogs.map((log) => {
+      const executor =
+        users.find((user) => user?.id === log?.userId) ||
+        responders.find((responder) => responder?.id === log?.userId) ||
+        admins.find((admin) => admin?.id === log?.userId);
 
-    const executor = users.find((user) => user?.id === log?.userId) ||
-                     responders.find((responder) => responder?.id === log?.userId) ||
-                     admins.find((admin) => admin?.id === log?.userId);
-  
-    const target = users.find((user) => user?.id === log?.targetId) ||
-                   responders.find((responder) => responder?.id === log?.targetId) ||
-                   admins.find((admin) => admin?.id === log?.targetId) || 
-                   announcement.find((announcement) => announcement?.id === log?.targetId) || 
-                   hotline.find((hotline) => hotline?.id === log?.targetId) ||
-                   clearance.find((clearance) => clearance?.id === log?.targetId) ||
-                   template.find((template) => template?.id === log?.targetId);
-  
-    return {
-      ...log,
-      fullname: executor?.fullname || "-",
-      userId: executor?.customId || "-",
-      type: executor ? (users.includes(executor) ? "resident" : responders.includes(executor) ? "responder" : "admin") : "-",
-      targetId: target?.customId || "-",  // Get custom ID for the target
-    };
-  });
-  
+      const target =
+        users.find((user) => user?.id === log?.targetId) ||
+        responders.find((responder) => responder?.id === log?.targetId) ||
+        admins.find((admin) => admin?.id === log?.targetId) ||
+        announcement.find(
+          (announcement) => announcement?.id === log?.targetId
+        ) ||
+        hotline.find((hotline) => hotline?.id === log?.targetId) ||
+        clearance.find((clearance) => clearance?.id === log?.targetId) ||
+        template.find((template) => template?.id === log?.targetId);
+
+      return {
+        ...log,
+        fullname: executor?.fullname || "-",
+        userId: executor?.customId || "-",
+        type: executor
+          ? users.includes(executor)
+            ? "resident"
+            : responders.includes(executor)
+            ? "responder"
+            : "admin"
+          : "-",
+        targetId: target?.customId || "-", // Get custom ID for the target
+      };
+    });
+  }, [
+    userlogs,
+    users,
+    responders,
+    admins,
+    announcement,
+    hotline,
+    clearance,
+    template,
+  ]);
+
   // After filtering by search query, further filter by action type
-// If filterData is "All" or empty, include all logs; otherwise, match specific action
-  const filteredData = useFilteredData(updatedData, searchQuery, searchFields).filter((log) => 
-  !filterData || filterData === "All" || log.action === filterData);
-  
-  const {currentPage,setCurrentPage, indexOfLastItem, indexOfFirstItem, currentItems, totalPages} = usePagination(filteredData)
+  // If filterData is "All" or empty, include all logs; otherwise, match specific action
+  const filteredData = useFilteredData(
+    updatedData,
+    searchQuery,
+    searchFields
+  ).filter(
+    (log) => !filterData || filterData === "All" || log.action === filterData
+  );
+
+  const {
+    currentPage,
+    setCurrentPage,
+    indexOfLastItem,
+    indexOfFirstItem,
+    currentItems,
+    totalPages,
+  } = usePagination(filteredData);
 
   const renderRow = (userLog) => {
-
-    const TdStyle = ({data}) => {
-        return (
-          <td className="px-2 py-2 sm:px-4 sm:py-4 text-xs sm:text-sm">
-          <div className="truncate max-w-[100px] sm:max-w-[200px]">
-            {data}
-          </div>
+    const TdStyle = ({ data }) => {
+      return (
+        <td className="px-2 py-2 sm:px-4 sm:py-4 text-xs sm:text-sm">
+          <div className="truncate max-w-[100px] sm:max-w-[200px]">{data}</div>
         </td>
-        )
-    }
-    return(
-       <>
-        <TdStyle data={userLog.userId}/>
-        <TdStyle data={userLog.fullname}/>
-        <TdStyle data={userLog.type}/>
-        <TdStyle data={formatDateWithTime(userLog.date)}/>
-        <TdStyle data={userLog.action}/>
-        <TdStyle data={userLog.targetId}/>
-       </>
-      
-    )
-  }
+      );
+    };
+    return (
+      <>
+        <TdStyle data={userLog.userId} />
+        <TdStyle data={userLog.fullname} />
+        <TdStyle data={userLog.type} />
+        <TdStyle data={formatDateWithTime(userLog.date)} />
+        <TdStyle data={userLog.action} />
+        <TdStyle data={userLog.targetId} />
+      </>
+    );
+  };
 
   return (
     <HeaderAndSideBar
@@ -100,7 +142,7 @@ const Audit = () => {
           <Toolbar
             label="Users Logs"
             buttons={
-              <SelectStyle 
+              <SelectStyle
                 value={filterData}
                 onChange={(e) => setFilterData(e.target.value)}
                 options={["All", ...actionLog]}
@@ -110,8 +152,13 @@ const Audit = () => {
             searchQuery={searchQuery}
             setSearchQuery={setSearchQuery}
           />
-          <Table headers={HeaderData} data={currentItems} renderRow={renderRow} emptyMessage="No user logs found" />
-          <Pagination 
+          <Table
+            headers={HeaderData}
+            data={currentItems}
+            renderRow={renderRow}
+            emptyMessage="No user logs found"
+          />
+          <Pagination
             currentPage={currentPage}
             totalPages={totalPages}
             setCurrentPage={setCurrentPage}
